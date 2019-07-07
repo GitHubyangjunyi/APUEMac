75f1643930980ccec81f4eb270a86641570ad310
杨俊艺 <1209032479@qq.com>
2019-07-08 00:32:58 +0800

    AAAAReadMe项目索引填写完毕sync还原点

--- AAAAReadMe/main.c
+++ AAAAReadMe/main.c
@@ -185,7 +185,7 @@ int main(int argc, const char * argv[]) {
 
 //项目索引
 //AAAAReadMe
-//Algorithm     算法
+//Algorithm         算法
     //Backtracking          回溯法
     //BranchAndBound        分支限界法
     //DivideAndConquer      分治法
@@ -196,164 +196,161 @@ int main(int argc, const char * argv[]) {
     //Iterative             迭代法
     //Recurrence            递推法
     //Recursion             递归法
-        //6//FibonacciRecursion 斐波那契数列低效率递归版
-        //5//ArrayReverseRecursion 数组倒置递归(未完成)
-        //0//ArraySumRecursion 数组求和,线性递归
-        //3//ItoaRecursion 通过递归调用将整数转换成字符串
-        //1//Printd 打印十进制数
-        //4//ReverseRecursion 递归版本的reverse函数,将字符串s倒置(相关TextProcessing/Reverse reverse函数,将字符串s倒置)
+        //5//ArrayReverseRecursion  数组倒置递归(未完成)
+        //0//ArraySumRecursion      数组求和,线性递归
+        //6//FibonacciRecursion     斐波那契数列低效率递归版
+        //3//ItoaRecursion          通过递归调用将整数转换成字符串
+        //1//Printd                 打印十进制数
+        //4//ReverseRecursion       递归版本的reverse函数,将字符串s倒置(相关TextProcessing/Reverse reverse函数,将字符串s倒置)
+//ArrayPointer      数组指针操作
 //BitsProcessing    位处理
     //假定最右边的一位是第0位,getbits(x,4,3)返回第4,3,2位,方向是从左到右,从左到右,从左到右
-
-    //5//BitCount 统计x中值为1的二进制位数 练习2-9
-    //1//Getbits 返回x中从第p位开始的n位
-    //3//Invert 返回对x执行下列操作后的结果值:将x中从第p位开始的n个位求反,x的其余各位保持不变 练习2-7
-    //4//Rightrot 返回将x循环右移n位后得到的值 练习2-8
-    //2//Setbits 返回对X执行下列操作后的结果值:将x中从第p位开始的n个位设置为y中最右边n位的值,x的其余各位保持不变 练习2-6
-//FileAPI       系统调用文件API
+    //5//BitCount           统计x中值为1的二进制位数 练习2-9
+    //1//Getbits            返回x中从第p位开始的n位
+    //3//Invert             返回对x执行下列操作后的结果值:将x中从第p位开始的n个位求反,x的其余各位保持不变 练习2-7
+    //4//Rightrot           返回将x循环右移n位后得到的值 练习2-8
+    //2//Setbits            返回对X执行下列操作后的结果值:将x中从第p位开始的n个位设置为y中最右边n位的值,x的其余各位保持不变 练习2-6
+//BlockStructure    作用域规则
+//BlockStructureFix 作用域规则static
+//BlockStructurePro 作用域规则之程序员的自我修养
+//Calculator        计算器
+    //2//CalculatorDivision 拆分版本
+    //1//CalculatorOne      初始版本
+//ConditionalCompilation    条件编译
+//ConstRestrict     const限定
+//EOFS              EOF测试
+//FileAPI           系统调用文件API
     //3//Close
     //4//Copy
     //5//CopySlow
     //8//Iotcl
-    //7//Open                  创建文件并测试文件的属性
-    //2//Read                  测试文件所具有的属性
+    //7//Open               创建文件并测试文件的属性
+    //2//Read               测试文件所具有的属性
     //6//Stat
     //1//Write
-//FileAPIStd    标准I/O库文件API
+//FileAPIStd        标准I/O库文件API
     //2//CopyStd
-    //3//ErgodicDir            递归遍历文件夹
-    //4//ErgodicDirImpru       递归遍历文件夹改进版本,可以通过命令行参数指定文件夹
+    //3//ErgodicDir         递归遍历文件夹
+    //4//ErgodicDirImpru    递归遍历文件夹改进版本,可以通过命令行参数指定文件夹
     //1//Fopen
-    //5//Mnapmini              内存映射函数文件共享
-//GNUCExtend            GNUC扩展
-    //11//Atttribute           特殊属性
-    //17//AtttributeAlias      函数别名
-    //13//AtttributeAligned    增大地址对齐
-    //18//AtttributeAlwaysInline 内联展开
-    //15//AtttributeFormat     变参函数的参数格式检查
-    //19//AtttributeNoinline   内联不展开
-    //14//AtttributePacked     减少地址对齐
-    //12//AtttributeSection    编译段指定
-    //16//AtttributeWeak       强符号转换为弱符号
-    //20//Builtin              内建函数
-    //22//BuiltinConstantp     内建函数编译优化
-    //23//BuiltinExpect        内建函数分支预测优化
-    //21//BuiltinFrameAddress  函数栈帧地址
-    //24//BuiltinLikelyUnLikely 内建函数分支预测优化
-    //7//ContainerOfMacro      内核第一宏使用示例
-    //6//ContainerOfMacroPre   内核第一宏解析
-    //0//CurrentFunctionName   获得当前函数名
-    //10//FlexibleArray        柔性数组成员
-    //29//GNUCExtendAmbiguity  可变参数宏歧义规避
-    //9//FlexibleArrayApply    柔性数组在USB驱动中的应用
-    //8//FlexibleArrayPre      柔性数组成员
-    //27//GNUCMacroExtend      可变参数宏的另一种写法
-    //28//GNUCMacroExtendApply 可变参数宏在内核中的应用
-    //26//GNUCMacroExtendFix   可变参数宏修正
-    //25//GNUCMacroExtendPre   可变参数宏简介
-    //30//LockMacro            实现锁机制的宏
-    //1//RangeMatch            范围匹配
-    //2//SpecifiedInitialization 指定初始化
-    //3//StatementExprs        语句表达式(宏构造利器)
-    //4//StatementExprsMacro   语句表达式宏
-    //5//Typeof                获得类型
-//InlineAssembleSet     内嵌汇编
-    //InlineAssemble 基本内联汇编寄存器约束
-    //InlineAssembleEight 内存约束损坏通知二&独占
-    //InlineAssembleEleven 机器模式hbwk
-    //InlineAssembleFive 名称占位符
-    //InlineAssembleFour 序号占位符之字长限定(机器模式相关)
-    //InlineAssembleNine 立即数约束iFIJNOX以及通用约束0到9
-    //InlineAssembleSeven 立即数约束损坏通知一m
-    //InlineAssembleSix 操作数类型修饰符=+&
-    //InlineAssembleTen 机器模式hbwk
-    //InlineAssembleThree 内存约束mo
-    //InlineAssembleTwo 寄存器约束abcdDSqrgAftu
-//LinkedList    链表
-    //6//DoubleWayLinkedList   双向链表(一个结点结构既有前驱地址又有后继地址)
-    //8//LinkedListPolynomial  单链表一元多项式计算(两个数据域的单链表,来自书本的部分函数还不是很完善,特殊情况无法最优化处理)
-    //5//CircleLinkedList      循环链表
-    //3//MallocTest            返回局部量的地址的函数测试用例
-    //4//MallocTestTwo         返回局部量的地址的函数测试用例
-    //9//MINEOSListTest        一个64位操作系统的设计与实现中lib.h里的list数据结构测试
-    //7//PrintSingleLinkedList 逆序输出单链表结点值测试用例
-    //1//SequentialList        顺序表(将线性表的元素依次存放在一个连续的存储空间中)
-    //2//SingleLinkedList      单链表(每个结点只有一个链域的链表)
-//MacroSet              宏定义相关
-    ////MacroDefinition 宏定义及宏定义的副作用
-    ////MacroNestedInstruction 宏嵌套指令,未完成
-    ////Macrosubstitution 宏替换
-    ////Va_list 可变长参数
-    ////Va_listCPlusPlus 可变长参数C++版
-//MemoryManagement      内存管理(主要收集了来自图灵社区 码农周刊第26期 经典C++ 中的关于内存管理的内容)
-    //5//Alloca 变长数组VLA
-    //3//Calloc 分配时清空内存的内存分配函数
-    //7//DanglingPointer 迷途指针(悬空指针)
-    //9//DanglingPointerThree 迷途指针(悬空指针)使用块语句产生的迷途指针,顺便提了下垃圾回收
-    //8//DanglingPointerTwo 迷途指针(悬空指针)更难察觉的迷途指针:一个以上的指针引用同一块内存区域而其中一个指针被释放
-    //10//ExceptionHandling 异常处理,资源获取即初始化RAII
-    //6//Free    重复释放/迷途指针(悬空指针)
-    //2//Malloc 堆管理器
-    //1//MemoryLeak 内存泄漏的各种姿势,过度分配,内存丢失跟踪,完全丢失地址,丢失起始地址,隐式泄漏
-    //0//NULLPointer 空指针的逻辑取反操作(来自知乎)
-    //4//Realloc    内存块重新分配
-//NumericalCalculation
-    ////Factorial 阶乘n!
-    ////Fibonacci 斐波那契数列
-    ////PIApproach PI逼近以及实数不能直接比相等
-    ////Polynomial 多项式计算(空间复杂度)
-    ////Power 计算整数base的n次幂
-    ////PythagoreanTriple 勾股数
+    //5//Mnapmini           内存映射函数文件共享
+//FirstDemo         C和指针第一章的Demo
+//FormatControl     格式控制
+//FunctionPointer   函数指针
+//GNUCExtend        GNUC扩展
+    //11//Atttribute            特殊属性
+    //17//AtttributeAlias       函数别名
+    //13//AtttributeAligned     增大地址对齐
+    //18//AtttributeAlwaysInline内联展开
+    //15//AtttributeFormat      变参函数的参数格式检查
+    //14//AtttributePacked      减少地址对齐
+    //12//AtttributeSection     编译段指定
+    //16//AtttributeWeak        强符号转换为弱符号
+    //19//Builtin               内建函数
+    //21//BuiltinConstantp      内建函数编译优化
+    //22//BuiltinExpect         内建函数分支预测优化
+    //20//BuiltinFrameAddress   函数栈帧地址
+    //23//BuiltinLikelyUnLikely 内建函数分支预测优化
+    //7//ContainerOfMacro       内核第一宏使用示例
+    //6//ContainerOfMacroPre    内核第一宏解析
+    //0//CurrentFunctionName    获得当前函数名
+    //10//FlexibleArray         柔性数组成员
+    //9//FlexibleArrayApply     柔性数组在USB驱动中的应用
+    //8//FlexibleArrayPre       柔性数组成员
+    //28//GNUCExtendAmbiguity   可变参数宏歧义规避
+    //26//GNUCMacroExtend       可变参数宏的另一种写法
+    //27//GNUCMacroExtendApply  可变参数宏在内核中的应用
+    //25//GNUCMacroExtendFix    可变参数宏修正
+    //24//GNUCMacroExtendPre    可变参数宏简介
+    //29//LockMacro             实现锁机制的宏
+    //1//RangeMatch             范围匹配
+    //2//SpecifiedInitialization指定初始化
+    //3//StatementExprs         语句表达式(宏构造利器)
+    //4//StatementExprsMacro    语句表达式宏
+    //5//Typeof                 获得类型
+//HighToLow         大小端问题
+//InlineAssembleSet 内嵌汇编
+    //InlineAssembleEight       内存约束损坏通知二&独占
+    //InlineAssembleEleven      机器模式hbwk
+    //InlineAssembleFive        名称占位符
+    //InlineAssembleFour        序号占位符之字长限定(机器模式相关)
+    //InlineAssembleNine        立即数约束iFIJNOX以及通用约束0到9
+    //InlineAssembleOne         基本内联汇编寄存器约束
+    //InlineAssembleSeven       立即数约束损坏通知一m
+    //InlineAssembleSix         操作数类型修饰符=+&
+    //InlineAssembleTen         机器模式hbwk
+    //InlineAssembleThree       内存约束mo
+    //InlineAssembleTwo         寄存器约束abcdDSqrgAftu
+//LinkedList        链表
+    //3//CircleLinkedList       循环链表
+    //4//DoubleWayLinkedList    双向链表(一个结点结构既有前驱地址又有后继地址)
+    //6//LinkedListPolynomial   单链表一元多项式计算(两个数据域的单链表,来自书本的部分函数还不是很完善,特殊情况无法最优化处理)
+    //7//MINEOSListTest         一个64位操作系统的设计与实现中lib.h里的list数据结构测试
+    //5//PrintSingleLinkedList  逆序输出单链表结点值测试用例
+    //1//SequentialList         顺序表(将线性表的元素依次存放在一个连续的存储空间中)
+    //2//SingleLinkedList       单链表(每个结点只有一个链域的链表)
+//MacroSet          宏定义相关
+    ////MacroDefinition         宏定义及宏定义的副作用
+    ////Macrosubstitution       宏替换
+    ////Va_list                 可变长参数
+    ////Va_listCPlusPlus        可变长参数C++版
+//MemoryManagement  内存管理(主要收集了来自图灵社区 码农周刊第26期 经典C++ 中的关于内存管理的内容)
+    //5//Alloca                 变长数组VLA
+    //3//Calloc                 分配时清空内存的内存分配函数
+    //7//DanglingPointer        迷途指针(悬空指针)
+    //9//DanglingPointerThree   迷途指针(悬空指针)使用块语句产生的迷途指针,顺便提了下垃圾回收
+    //8//DanglingPointerTwo     迷途指针(悬空指针)更难察觉的迷途指针:一个以上的指针引用同一块内存区域而其中一个指针被释放
+    //6//Free                   重复释放/迷途指针(悬空指针)
+    //2//Malloc                 堆管理器
+    //10//MallocTest            返回局部量的地址的函数测试用例
+    //11//MallocTestTwo         返回局部量的地址的函数测试用例
+    //1//MemoryLeak             内存泄漏的各种姿势,过度分配,内存丢失跟踪,完全丢失地址,丢失起始地址,隐式泄漏
+    //0//NULLPointer            空指针的逻辑取反操作(来自知乎)
+    //4//Realloc                内存块重新分配
+//Memset            内存块操作函数memset
+//NumericalCalculation  数值计算
+    ////Factorial               阶乘n!
+    ////Fibonacci               斐波那契数列
+    ////PIApproach              PI逼近以及实数不能直接比相等
+    ////Polynomial              多项式计算(空间复杂度)
+    ////Power                   计算整数base的n次幂
+    ////PythagoreanTriple       勾股数
+//OOPAbstract           面向对象方法
 //PerformanceTesting    性能测试
-//POSIXThread   POSIXT线程
-    //2//DeadLock              死锁
-    //1//MultiThreadCounter    多线程计数器冲突
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-//
-
-//二//Sort/QuickSort 快速排序
-//Search        搜索
-    //1//Binsearch 折半查找(数组v必须以升序排列) 练习3-1
-//Sort          排序算法
-    //2//QuickSort 快速排序(C.A.R.Hoare于1962年) P74
-    //1//ShellSort Shell排序(D.L.Shell于1959年) P51
+//POSIXThread           POSIXT线程
+    //2//DeadLock               死锁
+    //1//MultiThreadCounter     多线程计数器冲突
+    ////
+//Search                搜索
+    //1//Binsearch              折半查找(数组v必须以升序排列) 练习3-1
+//Sort                  排序算法
+    //QuickSort                 快速排序(C.A.R.Hoare于1962年) P74
+    //ShellSort                 Shell排序(D.L.Shell于1959年) P51
 //StackAndQueue         运算受限的线性表----栈和队列
-    //2//LinkStack 链栈
-    //1//SequentialStack 顺序栈
-    //3//StackConversion 数制转换
-    //5//StackNotation    表达式求值
-    //4//StackRecursion 栈实现递归
-
-    //5//LinkedQueue 链队列
-    //1//PascalTriangle 杨辉三角的队列解法
-    //2//PascalTriangleImprovement 杨辉三角的队列解法改进版(循环队列)
-    //3//PascalTriangleImprovementError 杨辉三角的队列解法改进版(循环队列)故意出错版,循环队列溢出
-    //6//PascalTriangleLinkedQueue 杨辉三角的链队列解法
-    //7//RadixSort 基数排序(桶排序)
-    //4//SequentialQueue 顺序队列(按循环队列规则操作)
-
-//环形队列相关算法是固定缓冲区读写的常用模式
-//StructureSet          结构
-    //5//BitField 位域
-    //1//StructPointers 指向结构的指针以及运算符->
-    //2//StructPointersLite 指向结构的指针以及运算符->简化版(来自C和指针 Kenneth A.Reek 著)
-    //3//StructPointersMajor 指向结构的指针以及运算符->专业版(来自知乎->与.运算符的区别,然而我把知乎的专业内容搬运到StructPointersLite那边了)
-    //4//StructurePuzzle *和->结合
-    //SystemEnvironment 系统环境信息的获取
-    //APUEMac                  如何写好一个main函数的参数验证//未修正
-    //1//ArgCheck              参数验证
-    //2//Getopt                参数获取函数//有偏差
-    //
-    //
-    //
-    //
-    //
+    //队列
+        //5//LinkedQueue        链队列
+        //1//PascalTriangle     杨辉三角的队列解法
+        //2//PascalTriangleImprovement      杨辉三角的队列解法改进版(循环队列)
+        //3//PascalTriangleImprovementError 杨辉三角的队列解法改进版(循环队列)故意出错版,循环队列溢出
+        //6//PascalTriangleLinkedQueue      杨辉三角的链队列解法
+        //7//RadixSort          基数排序(桶排序)
+        //4//SequentialQueue    顺序队列(按循环队列规则操作)
+    //栈
+        //2//LinkStack          链栈
+        //1//SequentialStack    顺序栈
+        //3//StackConversion    数制转换
+        //4//StackRecursion     栈实现递归
+    //环形队列相关算法是固定缓冲区读写的常用模式
+//StructureSet  结构
+    //5//BitField               位域
+    //1//StructPointers         指向结构的指针以及运算符->
+    //2//StructPointersLite     指向结构的指针以及运算符->简化版(来自C和指针 Kenneth A.Reek 著)
+    //3//StructPointersMajor    指向结构的指针以及运算符->专业版(来自知乎->与.运算符的区别,然而我把知乎的专业内容搬运到StructPointersLite那边了)
+    //4//StructurePuzzle        *和->结合
+//SystemEnvironment 系统环境信息的获取
+    //APUEMac                   如何写好一个main函数的参数验证//未修正
+    //1//ArgCheck               参数验证
+    //2//Getopt                 参数获取函数//有偏差
     //
 //TextProcessing    文本处理
     //标准库提供的输入/输出模型:
@@ -368,61 +365,50 @@ int main(int argc, const char * argv[]) {
     //且第一个Ctrl+Z作为ASCII码为26字符并入前面输入,并且在第一个Ctrl+Z后输入的字符和回车将不存入当前行,但输入回车有换行效果
     //Linux下用Ctrl+D
 
-    //37//Atof 将数字串s转换成相应的双精度浮点数,包括简单计算器程序 练习4-2
-    //25//Atoi 将数字串S转换成相应的整型数
+    //37//Atof                      将数字串s转换成相应的双精度浮点数,包括简单计算器程序 练习4-2
+    //25//Atoi                      将数字串S转换成相应的整型数
     //10//CharClassifiedStatistics  字符分类统计:统计各个数字,空白符及其他字符出现的次数
     //30//CharClassifiedStatisticsSwitch  字符分类统计:统计各个数字,空白符及其他字符出现的次数(Switch版) P48
-    //2//CharCount 字符统计
-    //23//DeleteNote 功能是删除注释 练习1-23
-    //20//Detab 将输入中的制表符替换成适当数目的空格,使空格充满到下一个制表符终止的地方 练习1-20(扩展练习5-11和5-12)
-    //21//Entab 将空格串替换为最少数量的制表符和空格,但要保持单词之间的间隔不变 练习1-21(扩展练习5-11和5-12)
-    //31//Escape escape(s,t)将字符串t复制到字符串s中,并在复制过程中将换行符,制表符等不可见字符分别转换成\t\n等相应的可见的转义字符序列,再写一个相反功能的函数 练习3-2
-    //6//EscapeCharacterRestore 转义字符还原 练习1-10
-    //32//Expand expand(s1,s2)将字符串s1中类似a-z一类的速记符号在字符串s2中扩展为完全等价的完整列表abc...xyz,该函数可以处理大小写,并且可以处理a-b-c,a-z0-9与-a-z等类似的情况,作为前导和尾随的-字符原样输出 练习3-3
-    //22//FoldLine 将较长的输入行折成短一些的两行或多行,折行的位置在输入行的第n列之前的最后一个非空格字符之后 练习1-22
-    //18//FormatTailBlanks 删除每个输入行末尾的空格及制表符,并删除完全是空格的行 练习1-18
-    //11//FrequencyHistogram 频度直方图:打印输入中各个字符出现频度的直方图 练习1-14
-    //12//Histogram 单词长度直方图(水平):打印输入中单词长度的直方图 练习1-13
-    //13//HistogramVersionTwo 单词长度直方图(垂直):打印输入中单词长度的直方图 练习1-13
-    //26//Htoi 把十六进制组成的字符串(包含可选的前缀0x或0X)转换为与之等价的整型值 练习2-3
-    //1//InputCopy 将输入复制到输出
-    //34//Itoa itoa函数将数字n转换为字符串并保存到s中,itoax函数可以接收三个参数,第三个参数为最小字段宽度,在必要时应在所得结果的左边填充一定的空格 练习3-4 3-6 P52
-    //33//Itob itob(n, s, b)将整数n转换为以b为底的数,并将转换结果以字符形式保存到字符串s中,b控制进制 练习3-5
-    //3//LineCount 行统计
-    //8//OneLineOneWord 每行一个单词打印输入 练习1-12
-    //9//OneLineOneWordVersionTwo 每行一个单词打印输入版本二
-    //17//PrintGreatInputLine 打印长度大于80个字符的所有输入行 练习1-17
-    //15//PrintTheLongestInputLine 打印最长输入行
-    //16//PrintTheLongestInputLineVersionTwo 打印最长输入行版本二,可以打印任意长度的输入行的长度,并尽可能多地打印文本 练习1-16
+    //2//CharCount                  字符统计
+    //23//DeleteNote                功能是删除注释 练习1-23
+    //20//Detab                     将输入中的制表符替换成适当数目的空格,使空格充满到下一个制表符终止的地方 练习1-20(扩展练习5-11和5-12)
+    //21//Entab                     将空格串替换为最少数量的制表符和空格,但要保持单词之间的间隔不变 练习1-21(扩展练习5-11和5-12)
+    //31//Escape                    escape(s,t)将字符串t复制到字符串s中,并在复制过程中将换行符,制表符等不可见字符分别转换成\t\n等相应的可见的转义字符序列,再写一个相反功能的函数 练习3-2
+    //6//EscapeCharacterRestore     转义字符还原 练习1-10
+    //32//Expand                    expand(s1,s2)将字符串s1中类似a-z一类的速记符号在字符串s2中扩展为完全等价的完整列表abc...xyz,该函数可以处理大小写,并且可以处理a-b-c,a-z0-9与-a-z等类似的情况,作为前导和尾随的-字符原样输出 练习3-3
+    //22//FoldLine                  将较长的输入行折成短一些的两行或多行,折行的位置在输入行的第n列之前的最后一个非空格字符之后 练习1-22
+    //18//FormatTailBlanks          删除每个输入行末尾的空格及制表符,并删除完全是空格的行 练习1-18
+    //11//FrequencyHistogram        频度直方图:打印输入中各个字符出现频度的直方图 练习1-14
+    //12//Histogram                 单词长度直方图(水平):打印输入中单词长度的直方图 练习1-13
+    //13//HistogramVersionTwo       单词长度直方图(垂直):打印输入中单词长度的直方图 练习1-13
+    //26//Htoi                      把十六进制组成的字符串(包含可选的前缀0x或0X)转换为与之等价的整型值 练习2-3
+    //1//InputCopy                  将输入复制到输出
+    //34//Itoa                      itoa函数将数字n转换为字符串并保存到s中,itoax函数可以接收三个参数,第三个参数为最小字段宽度,在必要时应在所得结果的左边填充一定的空格 练习3-4 3-6 P52
+    //33//Itob                      itob(n, s, b)将整数n转换为以b为底的数,并将转换结果以字符形式保存到字符串s中,b控制进制 练习3-5
+    //3//LineCount                  行统计
+    //8//OneLineOneWord             每行一个单词打印输入 练习1-12
+    //9//OneLineOneWordVersionTwo   每行一个单词打印输入版本二
+    //17//PrintGreatInputLine       打印长度大于80个字符的所有输入行 练习1-17
+    //15//PrintTheLongestInputLine  打印最长输入行
     //14//PrintTheLongestInputLineExtern 打印最长输入行(使用外部变量版,失去通用性)
-    //19//Reverse 将字符串S中的字符顺序颠倒过来,每次颠倒一个输入行中的字符顺序 练习1-19
-    //24//SignPairDetection 符号配对检测 练习1-24
-    //5//SpaceSubstitution 多空格用单空格替换 练习1-9
-    //27//Squeeze 从字符串S中删除字符C
-    //4//STNCount 空格,制表符以及换行符统计 练习1-8
-    //28//Strcat 将字符串T连接到字符串S的尾部,S必须有足够大的空间
-    //38//Strcmp strcmp函数返回值测试
-    //36//Strindex strindex(s,t)函数返回字符串t在字符串s中的出现的开始位置或索引,当s不包含t时返回-1 练习4-1
-    //29//Strpbrk 将字符串S2中的任一字符在字符串S1中第一次出现的位置作为结果返回,如果S1中不包含S2中的字符,返回-1  练习2-5
-    //35//Trim 删除字符串尾部的空格符,制表符和换行符 P53
-    //7//WordCount 单词数行数字符数统计
+    //16//PrintTheLongestInputLineVersionTwo 打印最长输入行版本二,可以打印任意长度的输入行的长度,并尽可能多地打印文本 练习1-16
+    //19//Reverse                   将字符串S中的字符顺序颠倒过来,每次颠倒一个输入行中的字符顺序 练习1-19
+    //24//SignPairDetection         符号配对检测 练习1-24
+    //5//SpaceSubstitution          多空格用单空格替换 练习1-9
+    //27//Squeeze                   从字符串S中删除字符C
+    //4//STNCount                   空格,制表符以及换行符统计 练习1-8
+    //28//Strcat                    将字符串T连接到字符串S的尾部,S必须有足够大的空间
+    //38//Strcmp                    strcmp函数返回值测试
+    //36//Strindex                  strindex(s,t)函数返回字符串t在字符串s中的出现的开始位置或索引,当s不包含t时返回-1 练习4-1
+    //29//Strpbrk                   将字符串S2中的任一字符在字符串S1中第一次出现的位置作为结果返回,如果S1中不包含S2中的字符,返回-1  练习2-5
+    //35//Trim                      删除字符串尾部的空格符,制表符和换行符 P53
+    //7//WordCount                  单词数行数字符数统计
 //Tree          树
-    //BinaryTree    二叉树
-    //HuffmanTree   哈夫曼树
-    //
-    //
-    //
-//Va_list           可变长参数列表
-//Va_listCPlusPlus  可变长参数列表C++
-    //
-    //
-    //
-    //
-    //
-    //
-//
-    //
-    //
-    //
-    //
-    //
+    ////BinaryTree          二叉树
+    ////HuffmanTree         哈夫曼树
+//TypeSystem    类型系统
+    //DataType              数据类型
+    //DetermineRanges       数据范围
+    //Enum                  枚举
+    //Typedef               类型定义
+    //UnionType             联合
